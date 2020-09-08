2a6e6cd9e7bd6bfc73f6f19583a74b2656bf71a5
Veronika1703 <akulova.veronika17@gmail.com>
2020-09-08 12:35:39 +0300

    added Smooth scroll and pageup

--- src/js/script.js
+++ src/js/script.js
@@ -92,5 +92,19 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+    //Smooth scroll and pageup
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 
 });
